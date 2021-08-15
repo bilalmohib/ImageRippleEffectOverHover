a22e9bf972f906e1752db3c0901be09cf63c9ee9
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-08-15 11:57:00 +0500

    feat

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 //Imoprting Components
 import Navbar from "./Navbar";
 import Player from "./Player";
+import RippleEffect from "./RippleEffect";
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <br />
-            <Player />
+            {/* <Player /> */}
+            <RippleEffect/>
         </>
     )
 }
---
+++ src/Components/RippleEffect.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styling/RippleEffect.css";
+import Scene from "../Photos/pic.jfif";
+
+const RippleEffect = () => {
+    return (
+        <div className="container">
+            {/* <img src={Scene} className="img-fluid img" alt="Responsive image" /> */}
+        </div>
+    )
+}
+export default RippleEffect;
